29f306072e6d0886cdd3135f329053fb03a80bae
Papa <pachou19@users.noreply.github.com>
2017-04-18 11:28:37 +0200

    Update, add some tips for debug

--- dotfiles/bin/smartcard_install.sh
+++ dotfiles/bin/smartcard_install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-sudo apt update
-
 preinstall() {
     sudo rm /etc/ssh/*key* moduli 
     sudo dpkg-reconfigure openssh-client 
     sudo dpkg-reconfigure openssh-server 
 }
 troubleshoot() {
+    #systemctl status pcscd # Must be turned on
     killall ssh-agent gpg-agent
     unset GPG_AGENT_INFO SSH_AGENT_PID SSH_AUTH_SOCK
     eval $(gpg-agent --daemon --enable-ssh-support)
     ssh-add -l
 }
 install_smartcard() {
+    sudo apt update
     preinstall
     echo "Installing Smartcard"
     sudo apt install -y libccid pcscd pcsc-tools gnupg-agent scdaemon
@@ -26,10 +26,11 @@ install_smartcard() {
     echo 'X-GNOME-Autostart-enabled=false' | cat /etc/xdg/autostart/gnome-keyring-gpg.desktop - > ~/.config/autostart/gnome-keyring-gpg.desktop
     cp /etc/xdg/autostart/gnome-keyring-ssh.desktop ~/.config/autostart/
     echo 'Hidden=true' >> ~/.config/autostart/gnome-keyring-ssh.desktop 
-    echo "eval \$(gpg-agent --daemon --enable-ssh-support)" | tee -a ~/.bashrc # Important to run at login
+    touch ~/.bashrc ~/.zshrc
+    echo "eval \$(gpg-agent --daemon --enable-ssh-support)" | tee -a ~/.bashrc ~/.zshrc # Important to run at login
     troubleshoot
     # $ ssh-add -l # to test
 }
 
-#preinstall
 install_smartcard
+#troubleshoot
