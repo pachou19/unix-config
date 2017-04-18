29fb5e1a7cef77d8fec576b60fe2d2b0b1667d8f
Papa <pachou19@users.noreply.github.com>
2017-04-18 11:42:31 +0200

    Create env.sh

---
+++ dotfiles/Projects/config/env.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+# Add env.sh # edit the .zshrc by opening the file in a text editor and adding the following below
+#source ~/Projects/config/env.sh
+
+# PATH
+#export PATH="/usr/local/share/python:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+export EDITOR='vim'
+# export PYTHONPATH=$PYTHONPATH
+# export MANPATH="/usr/local/man:$MANPATH"
+
+# Virtual Environment
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+source /usr/local/bin/virtualenvwrapper.sh
+
+# Owner
+#export USER_NAME="YOUR NAME"
+eval "$(rbenv init -)"
+
+# FileSearch
+function f() { find . -iname "*$1*" ${@:2} }
+function r() { grep "$1" ${@:2} -R . }
+
+#mkdir and cd
+function mkcd() { mkdir -p "$@" && cd "$_"; }
+
+# Aliases
+alias cppcompile='c++ -std=c++11 -stdlib=libc++'
+
+# Use sublimetext for editing config files
+alias zshconfig="subl ~/.zshrc"
+alias envconfig="subl ~/Projects/config/env.sh"
